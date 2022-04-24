85331e1593d44abfd0425b85a7250021ce152ab9
venkat2789 <venkatachalapathy.27@gmail.com>
2022-04-24 12:43:18 -0400

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Venkatachalapathy Sivaprakasam on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
